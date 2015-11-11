7656dba5e00e7ec3a4b06a8aecb38634ae78c848
solidet CHOURN <csolidet@gmail.com>
2015-11-11 15:08:48 +0700

    add fos_user bundle

--- Relax/app/AppKernel.php
+++ Relax/app/AppKernel.php
@@ -24,6 +24,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new AccommodationBundle\AccommodationBundle(),
+            new UserBundle\UserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
